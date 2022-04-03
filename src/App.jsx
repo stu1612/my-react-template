b664c03b7aab14d5446da9304851553742186b24
Stu Bolderson <stu.bolderson@aol.com>
2022-04-03 21:12:23 +0200

    re-add folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-// pages
-import Home from "./pages/Home";
+import Nav from "./components/Nav";
 // styles
 import "./styles/style.css";
 
 export default function App() {
   return (
     <div className="App">
-      <Home />
+      <Nav />
     </div>
   );
 }

31b4617f7df9e8c54b66f1db2a8509c2a0218889
Stu Bolderson <stu.bolderson@aol.com>
2022-04-03 20:57:04 +0200

    file cleanup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// pages
+import Home from "./pages/Home";
+// styles
+import "./styles/style.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}

321fcad49c5829b53a321542b007e7d5c2bcb7a2
Stu Bolderson <stu.bolderson@aol.com>
2022-04-04 16:27:29 +0200

    testing fix added to setuptests

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,24 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom';
+import "@testing-library/jest-dom";
+
+const originalError = console.error;
+beforeAll(() => {
+  console.error = (...args) => {
+    if (
+      /Warning: ReactDOM.render is no longer supported in React 18./.test(
+        args[0]
+      )
+    ) {
+      return;
+    }
+    originalError.call(console, ...args);
+  };
+});
+
+afterAll(() => {
+  console.error = originalError;
+});
+
+// globalThis.IS_REACT_ACT_ENVIRONMENT = true;
